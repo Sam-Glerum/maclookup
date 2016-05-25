a523db159ae4cc06224e9b2844b40eafd24d5631
Sam Glerum <samuelglerum@hotmail.com>
2016-05-25 15:52:03 +0200

    installed pep8, got rid of style-errors

--- samlookup.py
+++ samlookup.py
@@ -1,9 +1,11 @@
-#Sam Glerum
+import urllib2
+from bs4 import BeautifulSoup
+
 '''
 - Post mac_address to http://coffer.com/mac_find/?string=
 - Return mac_address + vendor
 '''
-#testmac = 00:13:A9
+# testmac = 00:13:A9
 '''
 Functions to be created:
 - Get user input
@@ -12,43 +14,43 @@ Functions to be created:
 - Read (scrape) the mac_address + right vendor
 - Print result to console
 '''
-from bs4 import BeautifulSoup
-import urllib2
-import requests
 
 
 class MacProgram(object):
-    def __init__(self):
+    """Practicing with classes, also looks better."""
+
+    def _init_(self):
         pass
 
     def user_input(self):
-        #get a mac address from the user
-        self.user_mac_input = str(raw_input("\nEnter a valid MAC-Address: ")).lower()
+        # get a mac address from the user
+        self.user_mac_input = str(raw_input(
+            "\nEnter a valid MAC-Address: ")).lower()
 
     def search_site(self):
-        #set url to search and append the mac address to the url
-        #this url can then be searched
+        # set url to search and append the mac address to the url
+        # this url can then be searched
         search_url = "http://coffer.com/mac_find/?string="
         mac_url = search_url + self.user_mac_input
         self.search_coffer = urllib2.urlopen(mac_url)
         return self.search_coffer
 
     def scrape_site(self):
-        #scrape the html-elements containing the mac_address + Vendor
-        #return the scraped data
+        # scrape the html-elements containing the mac_address + Vendor
+        # return the scraped data
         site_data = self.search_coffer
         soup = BeautifulSoup(site_data, "lxml")
 
-        #print mac address
-        mytables = soup.find_all("td", {"class" : "table2"})
+        # print mac address
+        mytables = soup.find_all("td", {"class": "table2"})
         for tr in mytables[:2]:
             print "MAC ADDRESS: {}".format(mytables[0].text)
 
-#create program object
+# create program object
 runprogram = MacProgram()
-#get user input
+# get user input
 runprogram.user_input()
-#post mac address to site
+# post mac address to site
 runprogram.search_site()
-#scrape content of site
+# scrape content of site
 runprogram.scrape_site()
